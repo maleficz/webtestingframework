b7d46da274b49d590882ed0388ac94a7d75703e6
Hanom1960 <hanom1960@cock.lu>
2016-09-15 23:02:22 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# TestingWeb Framework
+Un framework para realizar auditorias de seguridad web avanzadas, usando las mejores herramientas actuales junto a otras de codigo propio.
+
+Uso:
+
+python2 webframework.py
---
+++ webframework.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python2
+# encoding: utf-8
+# Testing Web Framework - Copyright (C) <2016> - <Hanom1960>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from modules import adminder
+from modules import inurlsearch
+from modules import sqlimod
+from modules import portsmod
+from modules import fingerwebmod
+from modules import checker
+
+def logo():
+    print """
+ _____          _   _             __    __     _     
+/__   \___  ___| |_(_)_ __   __ _/ / /\ \ \___| |__  
+  / /\/ _ \/ __| __| | '_ \ / _` \ \/  \/ / _ \ '_ \ 
+ / / |  __/\__ \ |_| | | | | (_| |\  /\  /  __/ |_) |
+ \/   \___||___/\__|_|_| |_|\__, | \/  \/ \___|_.__/ 
+                            |___/                    
+   ___                                            _    
+  / __\ __ __ _ _ __ ___   _____      _____  _ __| | __
+ / _\| '__/ _` | '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ /
+/ /  | | | (_| | | | | | |  __/\ V  V / (_) | |  |   < 
+\/   |_|  \__,_|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\
+
+
+	Coder: Hanom1960 Email/XMPP: hanom1960@cock.lu
+		Twitter: @hanomlulzsec
+"""
+
+def disclaimer():
+    print "[!] legal disclaimer: Usage of Testing Web Framework for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program"
+try:
+    logo()
+    print ""
+    disclaimer()
+    print ""
+    checker.check()
+    def webframework():
+        print ""
+        print """Select only one opcion.
+        a) Search for SQLi, LFI, RCE, XSS vulnerables URLs.
+        b) Make SQLi to a vulnerable web
+        c) Make a compleate scann to an host, enumerate DNS, Bypass Cloudflare y more.
+	d) Make web penetration tests & vulnerabilities analisys.
+        e) Search for the web site's admin panel.
+        f) Exit.
+        """
+        sel=raw_input("Select: ")
+        if sel == "a":
+            inurlsearch.execute()
+            webframework()
+        elif sel == "b":
+            sqlimod.execute()
+            webframework()
+        elif sel == "c":
+            portsmod.menu()
+            webframework()
+        elif sel == "d":
+            fingerwebmod.execute()
+            webframework()
+        elif sel == "e":
+            adminder.adminfind()
+            webframework()
+        elif sel == "f":
+            print "Exiting."
+        else:
+            webframework()
+    webframework()
+
+except KeyboardInterrupt:
+    print "Exiting."
+    pass
+os._exit(0)
